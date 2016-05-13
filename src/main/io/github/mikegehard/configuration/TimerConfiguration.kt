bdc7440a5f72dd518c664de4d65474ddd8a2e3f9
Mike Gehard <mgehard@pivotal.io>
2016-05-13 16:58:55 -0600

    Log some words every 5 seconds.

---
+++ src/main/io/github/mikegehard/configuration/TimerConfiguration.kt
@@ -0,0 +1,19 @@
+package io.github.mikegehard.configuration
+
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+
+@Configuration
+@EnableScheduling
+open class TimerConfiguration {
+    companion object {
+        val logger = LoggerFactory.getLogger(TimerConfiguration::class.java)
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    open fun doSomething() {
+        logger.info("Doing something every 5 seconds!")
+    }
+}
